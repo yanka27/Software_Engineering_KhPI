246d6936cc001ccdeaecb65f379b0f1e16860138
yanka27 <106449448+yanka27@users.noreply.github.com>
2022-12-16 17:16:53 +0200

    Update Lab 2.cpp

--- SE Lab/Lab 2.cpp
+++ SE Lab/Lab 2.cpp
@@ -3,59 +3,104 @@
 
 using namespace std;
 
-int main()
-{
-    int a, b, n;
-    double step;
+void inputA(double& a) {
+
     cout << "Input a: ";
-    cin >> a;
+
+    if (!(cin >> a)) {
+        throw "Incorrect data.";
+    }
+}
+
+void inputB(double& b, double& a) {
+
     cout << "Input b: ";
-    cin >> b;
-    cout << "Input step(should be positive): ";
-    cin >> step;
-    if (!(step > 0)) {
-        do {
-            cout << "Step should be positive: ";
-            cin >> step;
-        } while (!(step > 0));
-    }
-    cout << "Input n >= 4, n: ";
-    cin >> n;
-    if (!(n >= 4)) {
-        do {
-            cout << "N should be equal to or more than 4" << endl;
-            cout << "Input n: ";
-            cin >> n;
-        } while (!(n >= 4));
-    }
 
+    if (!(cin >> b)) {
+        throw "Incorrect data.";
+    }
+    if (a > b) {
+        throw "Error. 'b' must be lower than 'a'.";
+    }
+}
 
-    for (double x = a; x <= b; x += step) {
-        if (x >= 0) {
-            double mlt = 1;
-            double sum = 0;
-            for (int i = 0; i <= n - 1; i++) {
-                for (int j = 0; j <= n - 1; j++) {
-                    int j_0 = pow(j, 2);
-                    mlt *= x + i + j_0;
-                }
-                sum += mlt;
-                mlt = 1;
-            }
-            cout << "x = " << x << "  ";
-            cout << "y = " << sum << endl;
+void inputN(double& n) {
+
+    cout << "Input n (n >= 4): ";
+
+    if (!(cin >> n)) {
+        throw "Incorrect data.";
+    }
+    if (n < 4) {
+        throw "Error. 'n' cannot be lower than 4.";
+    }
+    if (!(n == (int)n)) {
+        throw "Error. 'n' must be an integer.";
+    }
+}
+
+void inputStep(double& step) {
+
+    cout << "Input step: ";
+
+    if (!(cin >> step)) {
+        throw "Incorrect data.";
+    }
+    if (step <= 0) {
+        throw "Error. 'step' must be higher than 0.";
+    }
+}
+
+double equation(double x, int n) {
+
+    double result;
+    double mlt = 1;
+    if (x < 0) {
+        double y = 0;
+        for (int j = 2; j <= n - 2; j++) {
+            mlt = 1;
+            mlt *= (j + 1);
         }
-        else if (x < 0) {
-            double mlt = 1;
-            double y = 0;
-            for (int j = 2; j <= n - 2; j++) {
-                mlt = 1;
-                mlt *= (j + 1);
+        y += mlt;
+        result = mlt;
+    }
+    else if (x >= 0) {
+        double sum = 0;
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j <= n - 1; j++) {
+                int j_0 = pow(j, 2);
+                mlt *= x + i + j_0;
             }
-            y += mlt;
-            cout << "x = " << x << "  ";
-            cout << "y = " << mlt << endl;
+            sum += mlt;
+            mlt = 1;
         }
+        result = sum;
+    }
+    return result;
+}
+
+void sequence(double& a, double& b, double n, double step) {
+
+    for (a; a <= b; a = a + step) {
+        cout << "x = " << a << " " << "y = " << equation(a, n) << endl;
+    }
+}
+
+int main() {
+
+    double a, b, n, step;
+    try {
+        inputA(a);
+        inputB(b, a);
+        inputN(n);
+        inputStep(step);
+        sequence(a, b, n, step);
+    }
+    catch (const char* message) {
+        cout << message << endl;
+    }
+    catch (...) {
+        cout << "Unknown error." << endl;
     }
     return 0;
 }
